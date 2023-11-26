8f0527709870532ac6818ac300e63158332bb6a5
Matias Toro <matias.toro@flextime.ie>
2023-11-26 08:12:46 +0100

    WIP

--- bindings/python/samples/index.sh
+++ bindings/python/samples/index.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
-sudo python3 positive-words.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
+sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
+
