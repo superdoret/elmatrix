35d721e0bf313b254f270a374ab415b346bfa43f
Matias Toro <matias.toro@flextime.ie>
2023-11-26 09:37:49 +0100

    WIP

--- bindings/python/samples/index.sh
+++ bindings/python/samples/index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
-sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
-sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --show-clock=True
+#sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
+#sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
+#sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --show-clock=True
 sudo python3 weather.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4
