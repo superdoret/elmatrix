6f0ce97cf0af7ee47a5d00a4347e1cc895badcab
Matias Toro <matias.toro@flextime.ie>
2023-11-26 13:45:00 +0100

    WIP

--- bindings/python/samples/index.sh
+++ bindings/python/samples/index.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
-#sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
-#sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
-#sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --show-clock=True --led-brightness 30
-#sudo python3 weather.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
-#sudo python3 commands.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
-#sudo python3 images.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
-sudo python3 tetris.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+while true
+do
+    action=$((1 + $RANDOM % 7))
+    echo $action
+
+    case $action in
+        1)
+            sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        2)
+            sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        3)
+            sudo python3 text.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --show-clock=True --led-brightness 30
+        ;;
+        4)
+            sudo python3 weather.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        5)
+            sudo python3 commands.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        6)
+            sudo python3 images.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        7)
+            sudo python3 tetris.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+        *)
+            sudo python3 positive-phrases.py --led-cols=64 --led-gpio-mapping=adafruit-hat --led-slowdown-gpio=4 --led-brightness 30
+        ;;
+    esac
+    sleep 2
+done
