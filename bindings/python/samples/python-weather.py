b44daa0325993a48743e9ed6a96285e3959b0d8d
Matias Toro <matias.e.toro@gmail.com>
2024-09-09 16:37:14 +0200

    feat(city): Add new location to whether

---
+++ bindings/python/samples/python-weather.py
@@ -0,0 +1,22 @@
+# import the module
+import python_weather
+
+import asyncio
+import os
+
+async def getweather():
+  # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+  async with python_weather.Client(unit=python_weather.METRIC) as client:
+    # fetch a weather forecast from a city
+    weather = await client.get('Barcelona')
+
+    # returns the current day's forecast temperature (int)
+    print(weather.temperature)
+
+if __name__ == '__main__':
+  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+  # for more details
+  if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+  asyncio.run(getweather())
