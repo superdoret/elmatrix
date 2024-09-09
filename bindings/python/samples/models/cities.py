ca19ac887d54838b2a3f412e04c657d6ab1177cf
Matias Toro <matias.e.toro@gmail.com>
2024-09-09 16:45:58 +0200

    feat(city): Add new location to whether

--- bindings/python/samples/models/cities.py
+++ bindings/python/samples/models/cities.py
@@ -3,12 +3,9 @@ import random
 
 class Cities():
     def get_random_one(self):
-        # cities = [
-        #     "Barcelona", "Rosario", "Madrid", "Copenhagen", "Rome", "Bologna", "Paris", "Alsace", "Zurich", "Sant Jordi"
-        # ]
-        cities = [
-            "Sant Jordi"
-        ]
+         cities = [
+             "Sant Jordi, Islas Baleares", "Barcelona", "Rosario", "Madrid", "Copenhagen", "Rome", "Bologna", "Paris", "Alsace", "Zurich"
+         ]
 
         pickup_position = random.randint(0,len(cities)-1)
         return cities[pickup_position]
