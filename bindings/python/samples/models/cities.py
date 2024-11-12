2839024bc00bf118961626fcb3ec61edff130d85
Matias Toro <matias.e.toro@gmail.com>
2024-11-12 08:25:44 +0100

    fix(city): remove sant jordi as city

--- bindings/python/samples/models/cities.py
+++ bindings/python/samples/models/cities.py
@@ -4,8 +4,8 @@ import random
 class Cities():
     def get_random_one(self):
          cities = [
-             "Sant Jordi, Islas Baleares", "Barcelona", "Rosario", "Madrid", "Copenhagen", "Rome", "Bologna", "Paris", "Alsace", "Zurich"
+             "Vinaros, Islas Baleares", "Barcelona", "Rosario", "Madrid", "Copenhagen", "Rome", "Bologna", "Paris", "Alsace", "Zurich"
          ]
 
-        pickup_position = random.randint(0,len(cities)-1)
-        return cities[pickup_position]
+    pickup_position = random.randint(0,len(cities)-1)
+    return cities[pickup_position]
